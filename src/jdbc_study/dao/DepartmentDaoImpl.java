af2eaed0d4bb37061f5b51f4ff814bdcd49c19ec
krilln <krilln@naver.com>
2018-11-09 16:29:50 +0900

    selectDepartmentByAll
    - try-resource-catch문으로 수정
    - getDepartment() 메서드 추가

--- src/jdbc_study/dao/DepartmentDaoImpl.java
+++ src/jdbc_study/dao/DepartmentDaoImpl.java
@@ -19,36 +19,23 @@ public class DepartmentDaoImpl implements DepartmentDao {
 	public List<Department> selectDepartmentByAll(){
 		List<Department> list = new ArrayList<>();
 		String sql = "select deptno, deptname, floor from department";
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			conn = MySQLjdbcUtil.getConnection();
-			pstmt = conn.prepareStatement(sql);
-			LOG.debug(pstmt);
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				int deptNo = rs.getInt("deptno");
-				String deptName = rs.getString("deptname");
-				int floor = rs.getInt("floor");
-				Department dept = new Department(deptNo, deptName, floor);
-				list.add(dept);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				rs.close();
-				pstmt.close();
-				conn.close();
-			} catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-			
+		try (Connection conn =  MySQLjdbcUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()){;
+				while(rs.next()) {
+					list.add(getDepartment(rs));
+				}
+		}catch (Exception e1) {
+			e1.printStackTrace();
 		}
-		
 		return list;
+	
+	}
+	private Department getDepartment(ResultSet rs) throws SQLException {
+		int deptNo = rs.getInt("deptno");
+		String deptName = rs.getString("deptname");
+		int floor = rs.getInt("floor");
+		return new Department(deptNo, deptName, floor);
 	}
 
 }
